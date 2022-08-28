79e2f6a149cc98d3811c45bdc703313558fb9ed6
Mutsuki Kimuraya <zaubermaerchen@me.com>
2022-08-29 01:32:40 +0900

    App.vue追加

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import IdolSelector from '@/components/IdolSelector.vue';
+import CostarChart from './components/CostarChart.vue';
+
+const name = ref('');
+onMounted(() => {
+  const parameters: URLSearchParams = new URLSearchParams(window.location.search);
+  name.value = parameters.get('name') ?? ''
+})
+</script>
+
+<template>
+  <IdolSelector v-model="name" />
+  <CostarChart v-if="name" :name="name" />
+</template>
