0944955f174408bc7665a9a56e5b4a59d1f8aaf3
Mutsuki Kimuraya <zaubermaerchen@me.com>
2022-08-29 11:42:15 +0900

    データ取得処理とグラフ表示を分けた

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import IdolSelector from '@/components/IdolSelector.vue'
 import CostarChart from './components/CostarChart.vue'
+import { getCartoonCostar } from '@/functions/api'
 
 const name = ref('')
+
+const data = ref<Api.CartoonCostar[]>([])
+const loadData = async() => {
+  const response = await getCartoonCostar(name.value)
+  response.sort((a, b) => Math.sign(b.count - a.count))
+  data.value = response
+}
+
 onMounted(() => {
   const parameters: URLSearchParams = new URLSearchParams(window.location.search)
   name.value = parameters.get('name') ?? ''
 })
+watch(name, () => void loadData())
 </script>
 
 <template>
   <IdolSelector v-model="name" />
-  <CostarChart v-if="name" :name="name" />
+  <CostarChart v-if="name" :data="data" />
 </template>
--- src/components/CostarChart.vue
+++ src/components/CostarChart.vue
@@ -1,21 +1,14 @@
 <script setup lang="ts">
-import {ref, computed, toRefs, onMounted, watch } from 'vue'
+import { computed } from 'vue'
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ChartOptions } from 'chart.js'
-import { getCartoonCostar } from '@/functions/api'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 interface Props {
-  name: string
+  data: Api.CartoonCostar[]
 }
 const props = defineProps<Props>()
-const { name } = toRefs(props)
-
-const response = ref<Api.GetCartoonCostarResponse | null>(null)
-const loadData = async() => {
-  response.value = await getCartoonCostar(name.value)
-}
 
 const options: ChartOptions = {
   indexAxis: 'y',
@@ -26,25 +19,16 @@ const options: ChartOptions = {
       position: 'top',
       beginAtZero: true,
       ticks: {
-        stepSize: 1
+        stepSize: 1,
       },
     },
   },
 }
-const data = computed(() => {
-  if (response.value === null) {
-    return null
-  }
-  const results = Array.from(response.value)
-
-  results.sort((a, b) => b.count - a.count)
-
-  return results
-})
 const dataSet = computed(() => {
+  const data = props.data?.slice(0, 50) ?? []
   const labels: string[] = []
   const values: number[] = []
-  for (const value of data?.value ?? []) {
+  for (const value of data) {
     labels.push(value.name)
     values.push(value.count)
   }
@@ -59,9 +43,6 @@ const dataSet = computed(() => {
     ]
   }
 })
-
-onMounted(() => void loadData())
-watch(name, () => void loadData())
 </script>
 
 <template>
