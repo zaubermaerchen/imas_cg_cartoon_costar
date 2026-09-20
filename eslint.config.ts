464e516483dd86e513eb029eb831180a001788c7
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2026-09-20 00:59:54 +0000

    eslint.config.tsに移行していなかった設定の対応

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,33 +1,40 @@
-import { globalIgnores } from "eslint/config";
-import {
-  defineConfigWithVueTs,
-  vueTsConfigs,
-} from "@vue/eslint-config-typescript";
-import pluginVue from "eslint-plugin-vue";
-import pluginVitest from "@vitest/eslint-plugin";
-import pluginOxlint from "eslint-plugin-oxlint";
-import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
-
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+import { globalIgnores } from 'eslint/config'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import pluginVitest from '@vitest/eslint-plugin'
+import pluginOxlint from 'eslint-plugin-oxlint'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfigWithVueTs(
   {
-    name: "app/files-to-lint",
-    files: ["**/*.{ts,mts,tsx,vue}"],
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
-  globalIgnores(["**/dist/**", "**/dist-ssr/**", "**/coverage/**"]),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-  pluginVue.configs["flat/essential"],
+  pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
 
   {
     ...pluginVitest.configs.recommended,
-    files: ["src/**/__tests__/*"],
+    files: ['src/**/__tests__/*'],
   },
-  ...pluginOxlint.configs["flat/recommended"],
+  ...pluginOxlint.configs['flat/recommended'],
   skipFormatting,
-);
+
+  {
+    name: 'app/custom-rules',
+    rules: {
+      indent: ['error', 2],
+      'vue/script-indent': ['error', 2],
+      'vue/html-indent': ['error', 2],
+      'no-undef': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/max-attributes-per-line': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/html-self-closing': ['error', { html: { void: 'always' } }],
+      '@typescript-eslint/no-namespace': 'off',
+    },
+  },
+)
